7d4cd89c2bd710215afc131bf26f1c7e807b8ced
Gurpreet <w0849359@myscc.ca>
2024-09-18 22:55:00 -0400

    Assignment 2

---
+++ MyData.ts
@@ -0,0 +1,26 @@
+export interface Car {
+  id: number;                  // unique ID of the car
+  madeBy: string;              // The manufacturer of the car (e.g., KIA)
+  model: string;               // Model of the car.
+  year: number;                // Year the car was manufactured
+  color?: string;              // Color of Car            --- Optional
+}
+const myCar: Car = {
+  id: 1,
+  madeBy: 'KIA',
+  model: 'Sports',
+  year: 2022,
+  color: 'Blue',
+};
+
+const anotherCar: Car = {
+  id: 2,
+  madeBy: 'Toyota',
+  model: 'Corolla',
+  year: 2021,
+
+};
+
+// Log the car objects to the console
+console.log('My Car:', myCar);
+console.log('Another Car:',anotherCar);
