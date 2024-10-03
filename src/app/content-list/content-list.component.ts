51d7f556b04bd8f374498eaa5bfe0d8c29fe8a77
Gurpreet <w0849359@myscc.ca>
2024-10-03 10:01:28 -0400

    Assignment 4 Commit , Working

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Car } from '../Shared Module/MyData';
 import {CarDetails} from "../Shared Module/carDetails";
 import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
+import {CarServiceService} from "../services/car-service.service";
 
 @Component({
   selector: 'app-content-list',
@@ -12,5 +13,17 @@ import {ContentListItemComponent} from "../content-list-item/content-list-item.c
   styleUrls: ['./content-list.component.css']
 })
 export class ContentListComponent {
+cars : CarDetails[] = [];
 
+constructor(private carService : CarServiceService) {
+
+}
+
+ngOnInit(){
+  this.carService.getCar().subscribe({
+    next: (data:CarDetails[]) => this.cars = data,
+    error:err => console.error("Error fetching Cars", err),
+    complete:() => console.log("Cars data fetch complete!")
+  })
+}
 }
